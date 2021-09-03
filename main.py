2000e778c03e377bc5994e971180d700378ad413
CGToxicBoy <86283864+CGToxicBoy@users.noreply.github.com>
2021-09-03 15:54:19 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+from random import randrange
+
+n = randrange(1000)
+
+while True:
+
+  v = int(input())
+
+if n == v:
+
+  print("You Win!")
+
+  breakpoint
+
+print('Smaller' if (n < y) else 'Bigger')
+
