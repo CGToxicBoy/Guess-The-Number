0e95329cfd1b1018fa9e7b1f329294ba66c9f330
CGToxicBoy <86283864+CGToxicBoy@users.noreply.github.com>
2021-09-07 21:52:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
-This program is under progress 
-pls wait for it to come soon
+import random
 
+top_of_range = input("Type a number: ")
+
+if top_of_range.isdigit():
+ top_of_range = int(top_of_range)
+
+ if top_of_range <= 0:
+   print("Pls write a number larger that 0 next time.")
+   quit()
+else: 
+  print("Please type a number next time.")
+  quit()
+
+random_number = random.randint(0, top_of_range)
+
+while True:
+  user_guess = input("Make a guess: ")
+  if user_guess.isdigit():
+     user_guess = int(user_guess)
+  else: 
+    print("Please type a number next time.")
+    continue
+
+  if user_guess == random.randint:
+   print("You got it!")
+else:
+  print("You got it wrong")
